28f8ae9744db1f3aa4f94b5b7227d47d623eebe6
manu022 <manuel.grandond@utem.cl>
2018-12-29 02:14:54 -0300

    ayer

--- data/flot-data.js
+++ data/flot-data.js
@@ -49,6 +49,38 @@ $(document).ready(function() {
     }
 });
 
+//ramos aprovados vs reprobados
+$(function() {
+
+  var data = [{
+    label: "Ramos aprovados",
+    data: 20
+  }, {
+    label: "Ramos reprobados",
+    data: 4
+  }];
+
+  var plotObj = $.plot($("#ap-vs-rep"), data, {
+    series: {
+        pie: {
+            show: true
+        }
+    },
+    grid: {
+        hoverable: true
+    },
+    tooltip: true,
+    tooltipOpts: {
+        content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
+        shifts: {
+            x: 20,
+            y: 0
+        },
+        defaultTheme: false
+    }
+  });
+});
+
 //Flot Pie Chart
 $(function() {
 
